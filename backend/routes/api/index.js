a62fd6a91dc9fc597cb9b2d8a1d3126a3fff3537
Armond Romeo <armondromeo@gmail.com>
2025-03-06 20:29:16 -0500

    Phase 4 Completed :)

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
 });
 
 // GET /api/set-token-cookie
@@ -40,5 +42,10 @@ router.get(
 );
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
 
 module.exports = router;
